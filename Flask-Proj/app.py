8984fdb65c88dbd3afaeaf4007b64367c0de0d7c
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-13 16:44:00 +0400

    :white_check_mark: Dynamic content rendering

--- Flask-Proj/app.py
+++ Flask-Proj/app.py
@@ -2,15 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Pikel Juice',
+        'title': 'Post 1',
+        'content': 'First post ever!!',
+        'date_posted': 'April 13, 2022',
+    },
+    {
+        'author': 'Jikel Puice',
+        'title': 'Post 2',
+        'content': 'Second post ever!!',
+        'date_posted': 'April 14, 2022',
+    },
+]
+
 
 @app.route("/")
 @app.route("/Home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/About")
 def about_info():
-    return render_template('about.html')
+    return render_template('about.html', title="About Us")
 
 
 if __name__ == '__main__':
