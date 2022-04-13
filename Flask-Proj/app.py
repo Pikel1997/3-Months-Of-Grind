8d4372a6ee8865161229213b8e2b28a00243d630
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-13 19:08:15 +0400

    :white_check_mark: Major changes, will be described i day2.md

--- Flask-Proj/app.py
+++ Flask-Proj/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = ''
 
 posts = [
     {
@@ -25,8 +28,29 @@ def home():
 
 
 @app.route("/About")
-def about_info():
+def about():
     return render_template('about.html', title="About Us")
+
+
+@app.route("/Register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/Login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
