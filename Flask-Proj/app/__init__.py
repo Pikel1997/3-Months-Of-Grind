09a3bc8c19aa07a9e2b57e2ecff0cf0350a12ab6
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-14 15:40:09 +0400

    :white_check_mark: Created database structure

--- Flask-Proj/app/__init__.py
+++ Flask-Proj/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '9c136116e31a4a50718787d0cc368cb7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
