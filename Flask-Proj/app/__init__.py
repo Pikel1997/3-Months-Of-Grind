a8fb158cf87ddc7d38a955c0ff4dde7406c6b572
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-14 15:50:34 +0400

    .env file setup

--- Flask-Proj/app/__init__.py
+++ Flask-Proj/app/__init__.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '9c136116e31a4a50718787d0cc368cb7'
+app.config['SECRET_KEY'] = os.getenv('token')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
