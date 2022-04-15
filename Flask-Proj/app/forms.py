e3f6572cecbad4a10f758de26d145aefa7c69c35
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-15 18:48:38 +0400

    :alien: added following func- multiple users, add post, add file, visit post link, fixed ui,

--- Flask-Proj/app/forms.py
+++ Flask-Proj/app/forms.py
@@ -1,28 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField\
+    username = StringField \
         ('Username',
          validators=[
              DataRequired(),
              Length(min=2, max=20)
          ])
-    email = StringField\
+    email = StringField \
         ('Email',
          validators=[
              DataRequired(),
              Email()
          ])
-    password = PasswordField\
+    password = PasswordField \
         ('Password',
          validators=[
              DataRequired()
          ])
-    confirm_password = PasswordField\
+    confirm_password = PasswordField \
         ('Confirm Password',
          validators=[
              DataRequired(),
@@ -42,16 +44,56 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField\
+    email = StringField \
         ('Email',
          validators=[
              DataRequired(),
              Email()
          ])
-    password = PasswordField\
+    password = PasswordField \
         ('Password',
          validators=[
              DataRequired()
          ])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField \
+        ('Username',
+         validators=[
+             DataRequired(),
+             Length(min=2, max=20)
+         ])
+    email = StringField \
+        ('Email',
+         validators=[
+             DataRequired(),
+             Email()
+         ])
+    picture = FileField \
+        ('Update Profile Picture',
+         validators=[
+             FileAllowed(['png', 'jpg'],
+                         "Files ending with .png and .jpg allowed only!")
+         ])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken please choose a different username')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken please use a different email')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- Flask-Proj/app/routes.py
+++ Flask-Proj/app/routes.py
@@ -1,28 +1,17 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'Pikel Juice',
-        'title': 'Post 1',
-        'content': 'First post ever!!',
-        'date_posted': 'April 13, 2022',
-    },
-    {
-        'author': 'Jikel Puice',
-        'title': 'Post 2',
-        'content': 'Second post ever!!',
-        'date_posted': 'April 14, 2022',
-    },
-]
 
 
 @app.route("/")
 @app.route("/Home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -68,7 +57,52 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/Account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route("/Account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/Post/New", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="New Post", form=form)
+
+
+@app.route("/Post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
