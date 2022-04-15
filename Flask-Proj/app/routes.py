a863a4a14e2a86978dd90e0396345d2bf655957d
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2022-04-15 20:35:04 +0400

    :white_check_mark: Added delete post functionality

--- Flask-Proj/app/routes.py
+++ Flask-Proj/app/routes.py
@@ -125,3 +125,15 @@ def update_post(post_id):
         form.title.data = post.title
         form.content.data = post.content
     return render_template('create_post.html', title="Update Post", form=form, legend='Update Post')
+
+
+@app.route("/Post/<int:post_id>/Delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
